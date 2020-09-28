54e8574cd2e1aa83c2d052cac2a45a8279027e5a
Miguel Hernandez <mhernand@thoughtworks.com>
2020-09-28 14:49:28 -0500

    Adds install Vault command

--- basic-setup-python.sh
+++ basic-setup-python.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash # vim:filetype=sh
 
-# xcode-select -p
-# xcode-select --install
 cd
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 brew cask install iterm2
 brew cask install docker
+brew cask install vault
 brew cask install jetbrains-toolbox
 brew cask install visual-studio-code
 pip3 install --upgrade pip --user
